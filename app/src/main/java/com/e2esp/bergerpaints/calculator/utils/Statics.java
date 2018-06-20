a20388375e3fb7631bf3f68e7605060479a9de48
zainqureshi1 <zain.qureshi.ly@gmail.com>
2018-06-20 12:04:59 +0500

    Updated prices, reordered paint arrays
    -> Updated paint prices to latest sheet
    -> Reordered the code related to setting paint data in arrays so that its easier now to find and update data of specific paints
    -> Moved drawables to correct folder

--- app/src/main/java/com/e2esp/bergerpaints/calculator/utils/Statics.java
+++ app/src/main/java/com/e2esp/bergerpaints/calculator/utils/Statics.java
@@ -189,7 +189,6 @@ public class Statics {
     * */
     public static void paint_list() {
 
-
         for (int i = 0; i < paint_Image_arry.length; i++) {
 
             paint_Image_arry[i] = new Paint();
@@ -209,450 +208,200 @@ public class Statics {
         }
 
         /*
-        * Set Images in classs
-        * */
-
-        paint_Image_arry[0].setImage_id(R.drawable.image1);
-
-        paint_Image_arry[1].setImage_id(R.drawable.image3);
-
-        paint_Image_arry[2].setImage_id(R.drawable.image7);
-
-        paint_Image_arry[3].setImage_id(R.drawable.image8);
-
-        paint_Image_arry[4].setImage_id(R.drawable.image9);
-
-        paint_Image_arry[5].setImage_id(R.drawable.image10);
-
-        paint_Image_arry[6].setImage_id(R.drawable.image11);
-
-        paint_Image_arry[7].setImage_id(R.drawable.image12);
-
-        paint_Image_arry[8].setImage_id(R.drawable.image13);
-
-        paint_Image_arry[9].setImage_id(R.drawable.image17);
-
-        paint_Image_arry[10].setImage_id(R.drawable.image5);
-
-        /*
-        * Door And Windows Paint
-        **/
-        doors_and_windows[0].setImage_id(R.drawable.image2);
-
-        doors_and_windows[1].setImage_id(R.drawable.image4);
-
-        doors_and_windows[2].setImage_id(R.drawable.image5);
-
-        doors_and_windows[3].setImage_id(R.drawable.image16);
-
-        /*
-        * exterior Paint
-        **/
-        exterior[0].setImage_id(R.drawable.image6);
-
-        exterior[2].setImage_id(R.drawable.image14);
-
-        exterior[1].setImage_id(R.drawable.image15);
-
-
-         /*
-        * wood finishes Paint
-        **/
-        wood_finishes[0].setImage_id(R.drawable.image18);
-
-        wood_finishes[1].setImage_id(R.drawable.image18);
-
-        wood_finishes[2].setImage_id(R.drawable.image18);
-
-        wood_finishes[3].setImage_id(R.drawable.image18);
-
-
-
-
-
-        /*
-        * Set Name In class
+        * Paint Array
         * */
 
         paint_Image_arry[0].setName("ELEGANCE  MATT EMULSION");
-
-        paint_Image_arry[1].setName("VIP PLASTIC EMULSION");
-
-        paint_Image_arry[2].setName("EAZY CLEAN EMULSION");
-
-        paint_Image_arry[3].setName("SPD SMOOTH EMULSION");
-
-        paint_Image_arry[4].setName("ECONOMY EMULSION");
-
-        paint_Image_arry[5].setName("SILK EMULSION");
-
-        paint_Image_arry[6].setName("MARBLEIZED FINISH");
-
-        paint_Image_arry[7].setName("GLAZED FINISH");
-
-        paint_Image_arry[8].setName("INTERIOR TEXTURE");
-
-        paint_Image_arry[9].setName("NU Emulsion");
-
-        paint_Image_arry[10].setName("ALLROUNDER MATT FINISH ENAMEL");
-        /*
-        * Door And Windows Paint
-         *  */
-        doors_and_windows[0].setName("ELEGANCE LONG LIFE ENAMEL");
-
-        doors_and_windows[1].setName("VIP SUPER GLOSS ENAMEL");
-
-        doors_and_windows[2].setName("ALLROUNDER MATT FINISH ENAMEL");
-
-        doors_and_windows[3].setName("NU Enamel");
-
-
-        /*
-        * exterior wall paint
-        * */
-        exterior[0].setName("WEATHER COAT");
-
-        exterior[2].setName("EXTERIOR TEXTURE");
-
-        exterior[1].setName("SAND FINISH");
-
-
-  /*
-        * wood finishes Paint
-         *  */
-        wood_finishes[0].setName("Timbercoat Polyurethane Color Varnish");
-
-        wood_finishes[1].setName("Timbercoat Acid Catalyzed Clear Matt Lacquer");
-
-        wood_finishes[2].setName("Timbercoat Clear Wood Lacquer");
-
-        wood_finishes[3].setName("Timbercoat Polyurethane Clear Wood Lacquer");
-
-
-
-
-        /*
-        * Set square Feet
-        *
-        * */
-
+        paint_Image_arry[0].setImage_id(R.drawable.image1);
         paint_Image_arry[0].setSquare_feet(161);
-
-        paint_Image_arry[1].setSquare_feet(151);
-
-        paint_Image_arry[2].setSquare_feet(151);
-
-        paint_Image_arry[3].setSquare_feet(140);
-
-        paint_Image_arry[4].setSquare_feet(140);
-
-        paint_Image_arry[5].setSquare_feet(200);
-
-        paint_Image_arry[6].setSquare_feet(32);
-
-        paint_Image_arry[7].setSquare_feet(350);
-
-        paint_Image_arry[8].setSquare_feet(16);
-
-        paint_Image_arry[9].setSquare_feet(151);
-
-        paint_Image_arry[10].setSquare_feet(151);
-
- /*
-        * Door And Windows Paint
-         *  */
-        doors_and_windows[0].setSquare_feet(151);
-
-        doors_and_windows[1].setSquare_feet(151);
-
-        doors_and_windows[2].setSquare_feet(151);
-
-        doors_and_windows[3].setSquare_feet(151);
-
- /*
-        * exterior Paint
-         *  */
-        exterior[0].setSquare_feet(151);
-
-        exterior[2].setSquare_feet(13);
-
-        exterior[1].setSquare_feet(16);
-
- /*
-        * wood finishes Paint
-         *  */
-        wood_finishes[0].setSquare_feet(118);
-
-        wood_finishes[1].setSquare_feet(118);
-
-        wood_finishes[2].setSquare_feet(118);
-
-        wood_finishes[3].setSquare_feet(118);
-
-
-
-
-/*
-*
-* Set metre Square
-* */
-
         paint_Image_arry[0].setMetre_square(15);
-
-        paint_Image_arry[1].setMetre_square(14);
-
-        paint_Image_arry[2].setMetre_square(14);
-
-        paint_Image_arry[3].setMetre_square(13);
-
-        paint_Image_arry[4].setMetre_square(13);
-
-        paint_Image_arry[5].setMetre_square(18.5);
-
-        paint_Image_arry[6].setMetre_square(3);
-
-        paint_Image_arry[7].setMetre_square(32.5);
-
-        paint_Image_arry[8].setMetre_square(1.5);
-
-        paint_Image_arry[9].setMetre_square(14);
-
-        paint_Image_arry[10].setMetre_square(14);
-
- /*
-        * Door And Windows Paint
-         *  */
-
-        doors_and_windows[0].setMetre_square(14);
-
-        doors_and_windows[1].setMetre_square(14);
-
-        doors_and_windows[2].setMetre_square(14);
-
-        doors_and_windows[3].setMetre_square(14);
-
-
-         /*
-        * exterior Paint
-         *  */
-
-        exterior[0].setMetre_square(14);
-
-        exterior[2].setMetre_square(1.25);
-
-        exterior[1].setMetre_square(1.5);
-
-
-
-
- /*
-        * wood finishes Paint
-         *  */
-
-        wood_finishes[0].setMetre_square(11);
-
-        wood_finishes[1].setMetre_square(11);
-
-        wood_finishes[2].setMetre_square(11);
-
-        wood_finishes[3].setMetre_square(11);
-
-
-
-/*
-*
-* Set price per Liter
-* */
-        paint_Image_arry[0].setP_p_liter(2559);
-
-        paint_Image_arry[1].setP_p_liter(2178);
-
-        paint_Image_arry[2].setP_p_liter(1420);
-
-        paint_Image_arry[3].setP_p_liter(1018);
-
-        paint_Image_arry[4].setP_p_liter(875);
-
-        paint_Image_arry[5].setP_p_liter(2951);
-
-        paint_Image_arry[6].setP_p_liter(1520);
-
-        paint_Image_arry[7].setP_p_liter(1020);
-
-        paint_Image_arry[8].setP_p_liter(2400);
-
-        paint_Image_arry[9].setP_p_liter(666);
-
-        paint_Image_arry[10].setP_p_liter(2606);
-
- /*
-        * Door And Windows Paint
-         *  */
-
-        doors_and_windows[0].setP_p_liter(2345);
-
-        doors_and_windows[1].setP_p_liter(2299);
-
-        doors_and_windows[2].setP_p_liter(2656);
-
-        doors_and_windows[3].setP_p_liter(1238);
-
- /*
-        *exterior Paint
-         *  */
-
-        exterior[0].setP_p_liter(2580);
-
-        exterior[2].setP_p_liter(3570);
-
-        exterior[1].setP_p_liter(4000);
-
-
-         /*
-        * wood finishes Paint
-         *  */
-
-        wood_finishes[0].setP_p_liter(788);
-
-        wood_finishes[1].setP_p_liter(1981);
-
-        wood_finishes[2].setP_p_liter(2009);
-
-        wood_finishes[3].setP_p_liter(2053);
-
-
-/**
- *
- * Set Liters
- * */
-
+        paint_Image_arry[0].setP_p_liter(2767);
         paint_Image_arry[0].setLiter(4);
-
-        paint_Image_arry[1].setLiter(4);
-
-        paint_Image_arry[2].setLiter(3.64);
-
-        paint_Image_arry[3].setLiter(3.64);
-
-        paint_Image_arry[4].setLiter(3.64);
-
-        paint_Image_arry[5].setLiter(4);
-
-        paint_Image_arry[6].setLiter(1);
-
-        paint_Image_arry[7].setLiter(0.5);
-
-        paint_Image_arry[8].setLiter(24);
-
-        paint_Image_arry[9].setLiter(3.64);
-
-        paint_Image_arry[10].setLiter(3.64);
-
-         /*
-        * Door And Windows Paint
-         *  */
-
-
-        doors_and_windows[0].setLiter(3.64);
-
-        doors_and_windows[1].setLiter(3.64);
-
-        doors_and_windows[2].setLiter(3.64);
-
-        doors_and_windows[3].setLiter(3.64);
-
-         /*
-        * exterior Paint
-         *  */
-
-
-        exterior[0].setLiter(4);
-
-        exterior[2].setLiter(20);
-
-        exterior[1].setLiter(20);
-
-
-   /*
-        * wood finishes Paint
-         *  */
-
-
-        wood_finishes[0].setLiter(0.91);
-
-        wood_finishes[1].setLiter(3.64);
-
-        wood_finishes[2].setLiter(3.64);
-
-        wood_finishes[3].setLiter(3.64);
-
-
-/**
- * Set Recommended coats
- * */
-
         paint_Image_arry[0].setRecommend_cots("2-3");
 
+        paint_Image_arry[1].setName("VIP PLASTIC EMULSION");
+        paint_Image_arry[1].setImage_id(R.drawable.image3);
+        paint_Image_arry[1].setSquare_feet(151);
+        paint_Image_arry[1].setMetre_square(14);
+        paint_Image_arry[1].setP_p_liter(2178);
+        paint_Image_arry[1].setLiter(4);
         paint_Image_arry[1].setRecommend_cots("2-3");
 
+        paint_Image_arry[2].setName("EAZY CLEAN EMULSION");
+        paint_Image_arry[2].setImage_id(R.drawable.image7);
+        paint_Image_arry[2].setSquare_feet(151);
+        paint_Image_arry[2].setMetre_square(14);
+        paint_Image_arry[2].setP_p_liter(1693);
+        paint_Image_arry[2].setLiter(3.64);
         paint_Image_arry[2].setRecommend_cots("2-3");
 
+        paint_Image_arry[3].setName("SPD SMOOTH EMULSION");
+        paint_Image_arry[3].setImage_id(R.drawable.image8);
+        paint_Image_arry[3].setSquare_feet(140);
+        paint_Image_arry[3].setMetre_square(13);
+        paint_Image_arry[3].setP_p_liter(1107);
+        paint_Image_arry[3].setLiter(3.64);
         paint_Image_arry[3].setRecommend_cots("2-3");
 
+        paint_Image_arry[4].setName("ECONOMY EMULSION");
+        paint_Image_arry[4].setImage_id(R.drawable.image9);
+        paint_Image_arry[4].setSquare_feet(140);
+        paint_Image_arry[4].setMetre_square(13);
+        paint_Image_arry[4].setP_p_liter(994);
+        paint_Image_arry[4].setLiter(3.64);
         paint_Image_arry[4].setRecommend_cots("2-3");
 
+        paint_Image_arry[5].setName("SILK EMULSION");
+        paint_Image_arry[5].setImage_id(R.drawable.image10);
+        paint_Image_arry[5].setSquare_feet(200);
+        paint_Image_arry[5].setMetre_square(18.5);
+        paint_Image_arry[5].setP_p_liter(3246);
+        paint_Image_arry[5].setLiter(4);
         paint_Image_arry[5].setRecommend_cots("2-3");
 
+        paint_Image_arry[6].setName("MARBLEIZED FINISH");
+        paint_Image_arry[6].setImage_id(R.drawable.image11);
+        paint_Image_arry[6].setSquare_feet(32);
+        paint_Image_arry[6].setMetre_square(3);
+        paint_Image_arry[6].setP_p_liter(1520);
+        paint_Image_arry[6].setLiter(1);
         paint_Image_arry[6].setRecommend_cots("3-4");
 
+        paint_Image_arry[7].setName("GLAZED FINISH");
+        paint_Image_arry[7].setImage_id(R.drawable.image12);
+        paint_Image_arry[7].setSquare_feet(350);
+        paint_Image_arry[7].setMetre_square(32.5);
+        paint_Image_arry[7].setP_p_liter(1020);
+        paint_Image_arry[7].setLiter(0.5);
         paint_Image_arry[7].setRecommend_cots("1");
 
+        paint_Image_arry[8].setName("INTERIOR TEXTURE");
+        paint_Image_arry[8].setImage_id(R.drawable.image13);
+        paint_Image_arry[8].setSquare_feet(16);
+        paint_Image_arry[8].setMetre_square(1.5);
+        paint_Image_arry[8].setP_p_liter(2400);
+        paint_Image_arry[8].setLiter(24);
         paint_Image_arry[8].setRecommend_cots("1");
 
+        paint_Image_arry[9].setName("NU Emulsion");
+        paint_Image_arry[9].setImage_id(R.drawable.image17);
+        paint_Image_arry[9].setSquare_feet(151);
+        paint_Image_arry[9].setMetre_square(14);
+        paint_Image_arry[9].setP_p_liter(746);
+        paint_Image_arry[9].setLiter(3.64);
         paint_Image_arry[9].setRecommend_cots("2-3");
 
+        paint_Image_arry[10].setName("ALLROUNDER MATT FINISH ENAMEL");
+        paint_Image_arry[10].setImage_id(R.drawable.image5);
+        paint_Image_arry[10].setSquare_feet(151);
+        paint_Image_arry[10].setMetre_square(14);
+        paint_Image_arry[10].setP_p_liter(2606);
+        paint_Image_arry[10].setLiter(3.64);
         paint_Image_arry[10].setRecommend_cots("2");
 
-         /*
-        * Door And Windows Paint
-         *  */
 
+        /*
+         * Door And Windows Paint
+         **/
 
+        doors_and_windows[0].setName("ELEGANCE LONG LIFE ENAMEL");
+        doors_and_windows[0].setImage_id(R.drawable.image2);
+        doors_and_windows[0].setSquare_feet(151);
+        doors_and_windows[0].setMetre_square(14);
+        doors_and_windows[0].setP_p_liter(2345);
+        doors_and_windows[0].setLiter(3.64);
         doors_and_windows[0].setRecommend_cots("2");
 
+        doors_and_windows[1].setName("VIP SUPER GLOSS ENAMEL");
+        doors_and_windows[1].setImage_id(R.drawable.image4);
+        doors_and_windows[1].setSquare_feet(151);
+        doors_and_windows[1].setMetre_square(14);
+        doors_and_windows[1].setP_p_liter(2633);
+        doors_and_windows[1].setLiter(3.64);
         doors_and_windows[1].setRecommend_cots("2-3");
 
+        doors_and_windows[2].setName("ALLROUNDER MATT FINISH ENAMEL");
+        doors_and_windows[2].setImage_id(R.drawable.image5);
+        doors_and_windows[2].setSquare_feet(151);
+        doors_and_windows[2].setMetre_square(14);
+        doors_and_windows[2].setP_p_liter(3011);
+        doors_and_windows[2].setLiter(3.64);
         doors_and_windows[2].setRecommend_cots("2");
 
+        doors_and_windows[3].setName("NU Enamel");
+        doors_and_windows[3].setImage_id(R.drawable.image16);
+        doors_and_windows[3].setSquare_feet(151);
+        doors_and_windows[3].setMetre_square(14);
+        doors_and_windows[3].setP_p_liter(1388);
+        doors_and_windows[3].setLiter(3.64);
         doors_and_windows[3].setRecommend_cots("2-3");
 
-         /*
-        * exterior Paint
-         *  */
 
+        /*
+         * Exterior Paint
+         **/
 
+        exterior[0].setName("WEATHER COAT");
+        exterior[0].setImage_id(R.drawable.image6);
+        exterior[0].setSquare_feet(151);
+        exterior[0].setMetre_square(14);
+        exterior[0].setP_p_liter(2833);
+        exterior[0].setLiter(4);
         exterior[0].setRecommend_cots("2-3");
 
+        exterior[2].setName("EXTERIOR TEXTURE");
+        exterior[2].setImage_id(R.drawable.image14);
+        exterior[2].setSquare_feet(13);
+        exterior[2].setMetre_square(1.25);
+        exterior[2].setP_p_liter(3570);
+        exterior[2].setLiter(20);
         exterior[2].setRecommend_cots("1");
 
+        exterior[1].setName("SAND FINISH");
+        exterior[1].setImage_id(R.drawable.image15);
+        exterior[1].setSquare_feet(16);
+        exterior[1].setMetre_square(1.5);
+        exterior[1].setP_p_liter(4000);
+        exterior[1].setLiter(20);
         exterior[1].setRecommend_cots("1");
 
 
+        /*
+         * Wood Finishes Paint
+         **/
 
-
-
-            /*
-        * wood finishes Paint
-         *  */
-
-
+        wood_finishes[0].setName("Timbercoat Polyurethane Color Varnish");
+        wood_finishes[0].setImage_id(R.drawable.image18);
+        wood_finishes[0].setSquare_feet(118);
+        wood_finishes[0].setMetre_square(11);
+        wood_finishes[0].setP_p_liter(789);
+        wood_finishes[0].setLiter(0.91);
         wood_finishes[0].setRecommend_cots("2-3");
 
+        wood_finishes[1].setName("Timbercoat Acid Catalyzed Clear Matt Lacquer");
+        wood_finishes[1].setImage_id(R.drawable.image18);
+        wood_finishes[1].setSquare_feet(118);
+        wood_finishes[1].setMetre_square(11);
+        wood_finishes[1].setP_p_liter(2285);
+        wood_finishes[1].setLiter(3.64);
         wood_finishes[1].setRecommend_cots("2-3");
 
+        wood_finishes[2].setName("Timbercoat Clear Wood Lacquer");
+        wood_finishes[2].setImage_id(R.drawable.image18);
+        wood_finishes[2].setSquare_feet(118);
+        wood_finishes[2].setMetre_square(11);
+        wood_finishes[2].setP_p_liter(2315);
+        wood_finishes[2].setLiter(3.64);
         wood_finishes[2].setRecommend_cots("2-3");
 
+        wood_finishes[3].setName("Timbercoat Polyurethane Clear Wood Lacquer");
+        wood_finishes[3].setImage_id(R.drawable.image18);
+        wood_finishes[3].setSquare_feet(118);
+        wood_finishes[3].setMetre_square(11);
+        wood_finishes[3].setP_p_liter(2363);
+        wood_finishes[3].setLiter(3.64);
         wood_finishes[3].setRecommend_cots("2-3");
+
     }
 
     /*
